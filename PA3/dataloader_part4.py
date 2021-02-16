ffe349f0adba787f1b474bde85696d32f9b929b7
ZunmingZhang <51297725+ZunmingZhang@users.noreply.github.com>
2021-02-15 22:15:45 -0800

    Update dataloader_part4.py

--- PA3/dataloader_part4.py
+++ PA3/dataloader_part4.py
@@ -17,33 +17,33 @@ Label = namedtuple('Label', [
 
 labels = [
     #       name                     level3Id  color
-    Label('road',    0, (128, 64, 128)),
-    Label('drivable fallback',    1, (81,  0, 81)),
-    Label('sidewalk',    2, (244, 35, 232)),
-    Label('non-drivable fallback',    3, (152, 251, 152)),
-    Label('person/animal',    4, (220, 20, 60)),
-    Label('rider',    5, (255,  0,  0)),
-    Label('motorcycle',    6, (0,  0, 230)),
-    Label('bicycle',   7, (119, 11, 32)),
-    Label('autorickshaw',   8, (255, 204, 54)),
-    Label('car',   9, (0,  0, 142)),
-    Label('truck',  10,  (0,  0, 70)),
-    Label('bus',  11,  (0, 60, 100)),
-    Label('vehicle fallback',  12,  (136, 143, 153)),
-    Label('curb',   13,  (220, 190, 40)),
-    Label('wall',  14,  (102, 102, 156)),
-    Label('fence',  15,  (190, 153, 153)),
-    Label('guard rail',  16,  (180, 165, 180)),
-    Label('billboard',   17,  (174, 64, 67)),
-    Label('traffic sign',  18,  (220, 220,  0)),
-    Label('traffic light',  19,  (250, 170, 30)),
-    Label('pole',  20,  (153, 153, 153)),
-    Label('obs-str-bar-fallback', 21,  (169, 187, 214)),
-    Label('building',  22,  (70, 70, 70)),
-    Label('bridge/tunnel',  23,  (150, 100, 100)),
-    Label('vegetation',  24,  (107, 142, 35)),
-    Label('sky',  25,  (70, 130, 180)),
-    Label('unlabeled',  26,  (0,  0,  0)),
+    Label('road', 0, (128, 64, 128)),
+    Label('drivable fallback', 1, (81, 0, 81)),
+    Label('sidewalk', 2, (244, 35, 232)),
+    Label('non-drivable fallback', 3, (152, 251, 152)),
+    Label('person/animal', 4, (220, 20, 60)),
+    Label('rider', 5, (255, 0, 0)),
+    Label('motorcycle', 6, (0, 0, 230)),
+    Label('bicycle', 7, (119, 11, 32)),
+    Label('autorickshaw', 8, (255, 204, 54)),
+    Label('car', 9, (0, 0, 142)),
+    Label('truck', 10, (0, 0, 70)),
+    Label('bus', 11, (0, 60, 100)),
+    Label('vehicle fallback', 12, (136, 143, 153)),
+    Label('curb', 13, (220, 190, 40)),
+    Label('wall', 14, (102, 102, 156)),
+    Label('fence', 15, (190, 153, 153)),
+    Label('guard rail', 16, (180, 165, 180)),
+    Label('billboard', 17, (174, 64, 67)),
+    Label('traffic sign', 18, (220, 220, 0)),
+    Label('traffic light', 19, (250, 170, 30)),
+    Label('pole', 20, (153, 153, 153)),
+    Label('obs-str-bar-fallback', 21, (169, 187, 214)),
+    Label('building', 22, (70, 70, 70)),
+    Label('bridge/tunnel', 23, (150, 100, 100)),
+    Label('vegetation', 24, (107, 142, 35)),
+    Label('sky', 25, (70, 130, 180)),
+    Label('unlabeled', 26, (0, 0, 0)),
 ]
 
 
@@ -54,13 +54,11 @@ class IddDataset(Dataset):
         self.n_class = n_class
         self.mode = csv_file
         self.transforms_ = transforms_
-        
+
         # Add mirror flip transformations
         self.fliplr = np.fliplr
         self.flipud = np.flipud
-        self.rotate = transforms.functional.rotate
-        
-        
+        self.rotate = transforms.RandomRotation((-90, 90), fill=26)
 
         # The following transformation normalizes each channel using the mean and std provided
         self.transforms = transforms.Compose([transforms.ToTensor(),
@@ -73,9 +71,9 @@ class IddDataset(Dataset):
 
     def __getitem__(self, idx):
 
-        img_name = self.data.iloc[idx, 0]
+        img_name = 'C:/Users/Zz3319/Desktop/idd20kII' + self.data.iloc[idx, 0]
         img = Image.open(img_name).convert('RGB')
-        label_name = self.data.iloc[idx, 1]
+        label_name = 'C:/Users/Zz3319/Desktop/idd20kII' + self.data.iloc[idx, 1]
         label = Image.open(label_name)
 
         img = self.crop(img)
@@ -86,12 +84,13 @@ class IddDataset(Dataset):
             label = self.fliplr(label)
             label = self.flipud(label)
         elif self.transforms_ == 'rotate':
-            img = self.rotate(img, 1)
-            label = self.rotate(label, 1)
+            degrees = self.rotate.get_params((-90, 90))
+            img = transforms.functional.rotate(img, degrees, fill=26)
+            label = transforms.functional.rotate(label, degrees, fill=26)
 
         img = self.transforms(np.asarray(img).copy()).float()  # Normalization
         label = torch.from_numpy(np.asarray(label).copy()).long()  # convert to tensor
-        
+
         return img, label
 
 #         # create one-hot encoding
